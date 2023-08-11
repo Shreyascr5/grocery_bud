1c81501ac8141e922d8de1180a428eebde8473d0
Shreyascr5 <shreyas050801@gmail.com>
2023-08-11 15:42:21 +0530

    resolved an error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
+  const[name,setName]=useState('');
+  const[list,setList]=useState([]);
+  const[isEditing,setIsEditing]=useState(false);
+  const[editID,setEditID]=useState(null);
+  const[alert,setAlert]=useState({
+    show:false,
+    msg:'',
+    type:''
+  })
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log("clicked")
+    if(!name){
+      //show alert
+    }
+    else if(name && isEditing){
+      //deal with edit
+    }
+    else{
+      //show alert
+      const newItem={id:new Date().getTime().toString(), title: name};
+      setList([...list,newItem]);
+      setName('');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className='section-center'>
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      {alert.show && <Alert/>}
+      <h3>Grocery bud </h3>
+      <div className="form-control">
+      <input type="text" className='grocery' placeholder='eg. Apples' value={name} onChange={(e)=>{
+        setName(e.target.value);
+      }}
+
+      />
+      <button type='submit' className='submit-btn'>
+        {isEditing? "Edit":"Submit"}
+      </button>
+      </div>
+    </form>
+    <div className="grocery-container">
+      <List items={list}/>
+      <button className='clear-btn'>clear items</button>
     </div>
+
+    </section>
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {FaEdit, FaTrash} from 'react-icons/fa'
+const List = ({items}) => {
+  return <div className='grocery-list'>{items.map((item)=>{
+    const {id,title}= item;
+    return(
+      <article className='grocery-item' key={id}>
+        <p className='title'>{title}</p>
+        <div className="bnt-container">
+          <button className='edit-btn' type='button'>
+            <FaEdit/>
+          </button>
+          <button className='delete-btn' type='button'>
+            <FaTrash/>
+          </button>
+        </div>
+      </article>
+    )
+  })}</div>
+}
+
+export default List
