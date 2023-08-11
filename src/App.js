68c209746fb7e452386d8b9574d4b857f12a3ce4
Shreyascr5 <shreyas050801@gmail.com>
2023-08-11 16:33:04 +0530

    started edit item feature

--- src/App.js
+++ src/App.js
@@ -42,6 +42,13 @@ function App() {
     showAlert(true,"danger","Grocery item removed")
     setList(list.filter((item)=> item.id!==id))
   }
+  const editItem=(id)=>{
+    const specificItem=list.find((item)=>item.id===id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+
+  }
   return (
     <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
@@ -59,7 +66,7 @@ function App() {
       </div>
     </form>
     {list.length>0 && <div className="grocery-container">
-      <List items={list} removeItem={removeItem}/>
+      <List items={list} removeItem={removeItem} editItem={editItem}/>
       <button className='clear-btn' onClick={clearList}>clear items</button>
     </div>}
     
--- src/List.js
+++ src/List.js
@@ -2,14 +2,14 @@ import React from 'react'
 import {FaEdit, FaTrash} from 'react-icons/fa'
 
 
-const List = ({items, removeItem}) => {
+const List = ({items, removeItem,editItem}) => {
   return <div className='grocery-list'>{items.map((item)=>{
     const {id,title}= item;
     return(
       <article className='grocery-item' key={id}>
         <p className='title'>{title}</p>
         <div className="bnt-container">
-          <button className='edit-btn' type='button'>
+          <button className='edit-btn' type='button' onClick={()=>{editItem(id)}}>
             <FaEdit/>
           </button>
           <button className='delete-btn'  onClick={()=>{
